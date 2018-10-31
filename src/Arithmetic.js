1857bce609d31f2f34e9c7d614d54df5957e36f3
Sergeenkov Mihail <sergeenkov.mihail@gmail.com>
2018-10-31 07:01:03 +0300

    Арифметические действия

---
+++ src/Arithmetic.js
@@ -0,0 +1,19 @@
+class Arithmetic {
+    static summation(x, y) {
+        return x + y;
+    }
+
+    static subtraction(x, y) {
+        return x - y;
+    }
+
+    static multiplication(x, y) {
+        return x * y;
+    }
+
+    static division(x, y) {
+        return x / y;
+    }
+}
+
+export default Arithmetic;
