a5dcecca280b28996f0778caf649dab263e18b9a
Sergeenkov Mihail <sergeenkov.mihail@gmail.com>
2018-10-31 07:01:03 +0300

    Арифметические действия

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+import Arithmetic from "~/src/Arithmetic";
+
+console.log(
+    Arithmetic.summation(1, 3),
+    Arithmetic.subtraction(1, 3),
+    Arithmetic.multiplication(1, 3),
+    Arithmetic.division(1, 3),
+    Arithmetic.division(1, 0)
+);
